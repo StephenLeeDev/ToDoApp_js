ade71ed962dea6d421600ffc2ac0e0586b45090d
StephenLeeDev <stephen.lee.dev@gmail.com>
2019-11-05 16:15:24 +0900

    Analyze this Project
    clock.js, bg.js is complete

--- bg.js
+++ bg.js
@@ -1,21 +1,24 @@
-const body = document.querySelector("body");
+const body = document.querySelector("body"); //Concatenate the constant body to the element body.
 
-const IMG_NUMBER = 3;
+const IMG_NUMBER = 3; //Number of images.
 
+//Draw background function.
 function paintImage(imgNumber) {
-  const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
-  image.classList.add("bgImage");
-  body.prepend(image);
+  const image = new Image(); //Create image object.
+  image.src = `images/${imgNumber + 1}.jpg`; //Select an image and set it on the object.
+  image.classList.add("bgImage"); //Set area of ​​image as background.
+  body.prepend(image); //Add image object to body(body is element).
 }
 
-function genRandom() {
+//Function to get random numbers from 1 to 3.
+function getRandom() {
   const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
 
+//function to get random numbers and draw background.
 function init() {
-  const randomNumber = genRandom();
+  const randomNumber = getRandom();
   paintImage(randomNumber);
 }
 
--- clock.js
+++ clock.js
@@ -1,16 +1,18 @@
-const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+const clockContainer = document.querySelector(".js-clock"), //Concatenate the constant clockContainer to the element .js-clock.
+  clockTitle = clockContainer.querySelector("h1"); //Concatenate the constant clockTitle to the element h1(h1 is a watch).
 
+//Function to get the current time.
 function getTime() {
-  const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
+  const date = new Date(); //Create date object.
+  const minutes = date.getMinutes(); //Get current minutes.
+  const hours = date.getHours(); //Get current hours.
+  const seconds = date.getSeconds(); //Get current seconds.
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  }:${seconds < 10 ? `0${seconds}` : seconds}`; //Enter the time on watch.
 }
 
+//Function to enter the time on the watch every second.
 function init() {
   getTime();
   setInterval(getTime, 1000);
