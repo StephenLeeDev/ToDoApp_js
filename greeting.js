b7e3124bc0c3aa58e8133e66265756e6288b60b5
StephenLeeDev <stephen.lee.dev@gmail.com>
2019-11-06 10:47:53 +0900

    Complete Analize

--- greeting.js
+++ greeting.js
@@ -1,37 +1,45 @@
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"), //Concatenate the constant form to the element .js-form.
+  input = form.querySelector("input"), //Concatenate the constant input to the element input.
+  greeting = document.querySelector(".js-greetings"); //Concatenate the constant greeting to the element .js-greeting.
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+//Function to store name at localstorage.
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
 
+//Function to handle save name, when clicked submit button.
 function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = input.value;
-  paintGreeting(currentValue);
-  saveName(currentValue);
+  const currentValue = input.value; //Get name.
+  paintGreeting(currentValue); //Greeting output.
+  saveName(currentValue); //Store name.
 }
 
+//Function to ask name.
 function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
+  form.classList.add(SHOWING_CN); //Add name field.
+  form.addEventListener("submit", handleSubmit); //Add name input button.
 }
 
+//Function to greeting output.
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
+  form.classList.remove(SHOWING_CN); //Remove name field.
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+  greeting.innerText = `Hello ${text}`; //Greeting.
 }
 
+//Function to load name.
 function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
+  const currentUser = localStorage.getItem(USER_LS); //Get name.
+  //If the name does not exist.
   if (currentUser === null) {
     askForName();
-  } else {
+  }
+  //Else the name exist.
+  else {
     paintGreeting(currentUser);
   }
 }
--- todo.js
+++ todo.js
@@ -1,11 +1,12 @@
-const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+const toDoForm = document.querySelector(".js-toDoForm"), //Concatenate the constant toDoForm to the element .js-toDoForm.
+  toDoInput = toDoForm.querySelector("input"), //Concatenate the constant toDoInput to the element input.
+  toDoList = document.querySelector(".js-toDoList"); //Concatenate the constant toDoList to the element .js-toDoList.
 
 const TODOS_LS = "toDos";
 
-let toDos = [];
+let toDos = []; //Array to store to do list.
 
+//Function to delete to do list.
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
@@ -14,42 +15,47 @@ function deleteToDo(event) {
     return toDo.id !== parseInt(li.id);
   });
   toDos = cleanToDos;
-  saveToDos();
+  saveToDos(); //Store to do list.
 }
 
+//Function to store to do list at localStorage.
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+//Function to output new item.
 function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
+  const li = document.createElement("li"); //Create item in list for to do item.
+  const delBtn = document.createElement("button"); //Create button for delete item.
+  const span = document.createElement("span"); //Create text area for output item's contents.
+  const newId = toDos.length + 1; //Index of where to store new item.
   delBtn.innerText = "❌";
-  delBtn.addEventListener("click", deleteToDo);
+  delBtn.addEventListener("click", deleteToDo); //Add delete function to delete button.
   span.innerText = text;
-  li.appendChild(delBtn);
-  li.appendChild(span);
-  li.id = newId;
-  toDoList.appendChild(li);
+  li.appendChild(delBtn); //Add new item's delete button to list.
+  li.appendChild(span); //Add new item's contents to list.
+  li.id = newId; //Set index of where to store new item.
+  toDoList.appendChild(li); //Add new item to list.
   const toDoObj = {
     text: text,
     id: newId
   };
-  toDos.push(toDoObj);
-  saveToDos();
+  toDos.push(toDoObj); //Add new item to list for store to localStorage.
+  saveToDos(); //Store new list to localStorage.
 }
 
+//Function to handle store to do item.
 function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
-  toDoInput.value = "";
+  const currentValue = toDoInput.value; //Get to do contents.
+  paintToDo(currentValue); //Output new item's contents.
+  toDoInput.value = ""; //Reset input field.
 }
 
+//Function to get to do list from localStorage.
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
+  //If a list exists, output it.
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function(toDo) {
